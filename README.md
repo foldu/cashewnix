30ae589578e90776fd443e2b4d206d222210c4ee
foldu <foldu@protonmail.com>
2024-05-24 12:45:07 +0200

    Improve module and add basic documentation to README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## cashewnix
+# cashewnix
 A binary cache proxy that can fetch from other binary caches discovered on the local network.
 
 Discovery works by first finding all interfaces that appear to be local networks and then
@@ -9,7 +9,69 @@ must have their clocks somewhat synced, because all signed messages include a ti
 to avoid replay attacks.
 
 ### Usage
-TODO
+This program is usable from the NixOS module included in this flake.
+```nix
+{ inputs, ... }:
+{
+  # ...
+  imports = [ inputs.cashewnix.nixosModules.cashewnix ];
+  # ...
+}
+```
+You first need to generate a public/private keypair with:
+```sh
+nix-store --generate-binary-cache-key example.org ./cashewnix-private ./cashewnix-public
+```
+Where `example.org` is just an identifier put in front of the key. Put the private key
+somewhere only readable by root. This key is used for both verifying the authenticity
+of devices advertising on the local network and signing derivations. If you're lazy
+you could re-use the same key on all devices in the network. The public key must
+be added to the configuration of all devices, otherwise this node will be ignored because
+it can't authenticate itself.
+
+An example for a minimal configuration which shares the local store
+with the network:
+```nix
+services.cashewnix = {
+  enable = true;
+  enableNixServe = true;
+  privateKeyPath = "/var/secrets/cashewnix-private";
+  settings.public_keys = [ 
+    "cache.example.org-1:2asqQ4huy7+QY5Ll45TyVtFkmGYdwKzyUW/vODwJDk8="
+  ];
+};
+
+services.nix-serve.openFirewall = true;
+```
+
+A configuration that can pull from other nodes but doesn't
+share its own store:
+```nix
+services.cashewnix = {
+  enable = true;
+  settings.public_keys = [
+    "cache.example.org-1:2asqQ4huy7+QY5Ll45TyVtFkmGYdwKzyUW/vODwJDk8="
+  ];
+};
+```
+If you're already hosting a binary cache on https:
+```nix
+services.cashewnix = {
+  enable = true;
+  privateKeyPath = "/var/secrets/cashewnix-private";
+  settings = {
+    public_keys = [
+      "cache.example.org-1:2asqQ4huy7+QY5Ll45TyVtFkmGYdwKzyUW/vODwJDk8="
+    ];
+    local_binary_caches.local_cache = {
+      advertise = "url";
+      url = "https://my-binary-cache.tld";
+    };
+  };
+};
+```
+
+For other options look inside [nix/module.nix].
 
 ### Prior work
 - [peerix](https://github.com/cid-chan/peerix)
--- nix/module.nix
+++ nix/module.nix
@@ -9,11 +9,18 @@ let
   cfg = config.services.cashewnix;
   types = lib.types;
   json = pkgs.formats.json { };
-  configFile = json.generate "cashewnix.json" cfg.settings;
+  settings =
+    if cfg.settings.local_binary_caches.local_cache == null && cfg.enableNixServe then
+      (lib.recursiveUpdate cfg.settings {
+        local_binary_caches.local_cache = {
+          advertise = "ip";
+          port = config.services.nix-serve.port;
+        };
+      })
+    else
+      cfg.settings;
+  configFile = json.generate "cashewnix.json" settings;
 in
-# TODO: make module easier to use
-# merge various fields into settings
-# TODO: document module properly
 {
   options.services.cashewnix = {
     enable = lib.mkEnableOption "Enables cashewnix local binary cache";
@@ -21,26 +28,37 @@ in
     openFirewall = lib.mkOption {
       type = types.bool;
       default = true;
-      description = ''
+      description = lib.mdDoc ''
         Open needed UDP port 49745 on all interfaces.
       '';
     };
 
     privateKeyPath = lib.mkOption {
       type = types.nullOr types.str;
+      example = "/var/secrets/cashewnix-private";
       default = null;
+      description = lib.mdDoc ''
+        Path to the private key. This is shared with nix-serve.
+      '';
     };
 
     settings = lib.mkOption {
       type = lib.types.submodule {
         freeformType = json.type;
-
         options = {
           port = lib.mkOption {
             type = lib.types.port;
             default = 9543;
             description = lib.mdDoc ''
               Port to bind to.
+            '';
+          };
+          public_keys = lib.mkOption {
+            type = lib.types.listOf lib.types.str;
+            example = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+            default = [ ];
+            description = lib.mdDoc ''
+              List of accepted public keys.
             '';
           };
           priority = lib.mkOption {
@@ -51,38 +69,78 @@ in
               0 while everything above is lower. Default cache.nixos.org priority is 40.
             '';
           };
+          local_binary_caches = lib.mkOption {
+            type = types.submodule {
+              options = {
+                discovery_refresh_time = lib.mkOption {
+                  type = types.str;
+                  default = "60s";
+                  description = lib.mdDoc ''
+                    Frequency with which to update the local binary caches.
+                  '';
+                };
+                local_cache = lib.mkOption {
+                  type = types.nullOr types.anything;
+                  example = {
+                    advertise = "url";
+                    url = "https://my-localcache.tld";
+                  };
+                  default = null;
+                  description = lib.mdDoc ''
+                    Configuration of the advertised local cache. Can be either
+                    `ip` with an associated port or `url` with an url.
+                  '';
+                };
+              };
+            };
+            default = { };
+          };
+          binary_caches = lib.mkOption {
+            type = lib.types.listOf lib.types.anything;
+            default = [ ];
+            example = [
+              {
+                url = "https://cache.nixos.org";
+                priority = "40";
+              }
+            ];
+            description = lib.mdDoc ''
+              Additional list of binary caches cashewnix can use.
+            '';
+          };
+          priority_config = lib.mkOption {
+            type = lib.types.anything;
+            default = {
+              "0" = {
+                timeout = "500ms";
+              };
+            };
+            description = lib.mdDoc ''
+              Configuration of priority buckets. Priority 0 is
+              the priority for local caches.
+            '';
+          };
         };
       };
     };
 
-    # TODO: use a submodule declaration
-
     enableNixServe = lib.mkOption {
       type = types.bool;
       default = false;
-      description = ''
+      description = lib.mdDoc ''
         Enable nix-serve to serve packages to the network.
-      '';
-    };
-
-    openNixServeFirewall = lib.mkOption {
-      type = types.bool;
-      default = false;
-      description = lib.mdDoc ''
-        Open nix-serve port.
-      '';
-    };
-
-    nixServePort = lib.mkOption {
-      type = types.port;
-      default = 4539;
-      description = lib.mdDoc ''
-        Port nix-serve listens on.
       '';
     };
   };
 
   config = lib.mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = !cfg.enableNixServe || (cfg.privateKeyPath != null);
+        message = "privateKeyPath must be defined if nix-serve is enabled";
+      }
+    ];
+
     networking.firewall.allowedUDPPorts = lib.mkIf cfg.openFirewall [ 49745 ];
 
     systemd.services.cashewnix =
@@ -125,11 +183,9 @@ in
       };
     };
 
-    services.nix-serve = {
-      enable = cfg.enableNixServe;
-      port = cfg.nixServePort;
+    services.nix-serve = lib.mkIf cfg.enableNixServe {
+      enable = true;
       secretKeyFile = cfg.privateKeyPath;
-      openFirewall = cfg.openNixServeFirewall;
       # original nix-serve is way too slow and will cause errors
       package = pkgs.nix-serve-ng;
     };
